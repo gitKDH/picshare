a760698dd93fd8f7ef4fbe1b29bc337b47d0aa60
gitKDH <reg0212@naver.com>
2023-05-28 14:36:26 +0900

    Initial commit

---
+++ src/test/java/com/example/picshare/PicshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
