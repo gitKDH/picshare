22ae4bfc605cef95556b9aa4458b555b0a81644c
gitKDH <reg0212@naver.com>
2023-05-31 16:33:18 +0900

    Member, MemberFormDto 추가

---
+++ src/main/java/com/example/theme_market/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.theme_market.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+//등록자, 수정자 자동으로 감시
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;       //등록자
+
+    @LastModifiedBy
+    private String modifiedBy;      //수정자
+
+}
+
