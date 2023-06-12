d501eea4da0d779f7f289b7d553bf4025cf74381
gitKDH <reg0212@naver.com>
2023-06-12 22:35:23 +0900

    이미지 리프레시 실패

--- src/main/java/com/example/theme_market/config/WebMvcConfig.java
+++ src/main/java/com/example/theme_market/config/WebMvcConfig.java
@@ -1,12 +1,16 @@
 package com.example.theme_market.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
+@PropertySource("classpath:application.properties")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${uploadPath}")
--- src/main/java/com/example/theme_market/repository/ItemRepository.java
+++ src/main/java/com/example/theme_market/repository/ItemRepository.java
@@ -21,14 +21,8 @@ public interface ItemRepository extends JpaRepository<Item, Long>,
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
 
-
-
     @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
-
-    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc",
-            nativeQuery = true)
-    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
     //상품 삭제
     @Modifying
